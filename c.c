44b8b43d5bb658b039ea8881be02b0949de22ba9
root <Petya228Kvas>
2025-09-07 21:37:11 +0700

    22

---
+++ c.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+void task1(void);
+void task2(void);
+void task3(void);
+void task4(void);
+void task5(void);
+void task6(void);
+void task7(void);
+int main(void){
+    task1();
+    task2();
+    task3();
+    task4();
+    task5();
+    task6();
+    task7();
+    return 0;
+}
+void task1(void){
+    printf("Пётр Гаврилов\n");
+    printf("Пётр\nГаврилов\n");
+    printf("Пётр ");printf("Гаврилов\n");
+}
+
